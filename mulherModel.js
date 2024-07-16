d6cf4766d3d3752b24b80d8ea2c5ca4720cb45a8
milena-spiazzi <milrnaspiazzi@gmail.com>
2024-07-16 12:27:19 -0300

    correção model

--- mulherModel.js
+++ mulherModel.js
@@ -11,7 +11,7 @@ const MulherSchema = new mongoose.Schema({
     },
     citacao: {
         type: String,
-        required: trusted
+        required: true
     },
     minibio: {
         type: String,
